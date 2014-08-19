5cdbf1a3a3dd2ce59620e399a91d97a82d238ccb
chameleonbr <chameleonbr@gmail.com>
2014-08-19 17:27:42 -0300

    Working now

---
+++ src/Extension/Text.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace ApiBird\Extension;
+
+class Text implements \ApiBird\ExtensionInterface
+{
+
+    public static $types = [
+        'plain/text'
+    ];
+
+    public function fromFormat($data)
+    {
+        return $data;
+    }
+
+    public function toFormat($data)
+    {
+        return $data;
+    }
+
+    public static function getTypes()
+    {
+        return static::$types;
+    }
+
+}
