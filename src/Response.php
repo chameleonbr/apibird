c8a747a8fc59b01618250f2fee43c00734574183
avila <avila@localhost>
2014-09-05 01:45:57 -0300

    Added some response functions.

--- src/Response.php
+++ src/Response.php
@@ -10,13 +10,97 @@ class Response extends \Phalcon\Http\Response
      * @param type $types
      */
     public function apiSend($app)
-    {    
+    {
         $di = $this->getDI();
         $ext = $di['request']->getBestAccept();
         $this->setHeader('Content-Type', $ext);
         $handler = $di['apibird']->getResponseExtension($ext);
         $this->setContent($handler->toFormat($app->getReturnedValue()));
         return $this->sendHeaders()->send();
+    }
+
+    public function ok($data = [], $headers = [])
+    {
+        $di = $this->getDI();
+        $ext = $di['request']->getBestAccept();
+        $this->setHeader('Content-Type', $ext);
+        $handler = $di['apibird']->getResponseExtension($ext);
+        $this->setContent($handler->toFormat($data));
+        return $this->sendHeaders()->send();
+    }
+
+    public function created($data = [], $headers = [])
+    {
+        
+    }
+
+    public function accepted($data = [], $headers = [])
+    {
+        
+    }
+
+    public function noContent($headers = [])
+    {
+        
+    }
+    
+    public function partialContent($data = [], $headers = [])
+    {
+        
+    }
+    
+    public function badRequest($data = [], $headers = [])
+    {
+        
+    }
+
+    public function unauthorized($data = [], $headers = [])
+    {
+        
+    }
+    public function paymentRequired($data = [], $headers = [])
+    {
+        
+    }
+    public function forbidden($data = [], $headers = [])
+    {
+        
+    }
+    public function notFound($data = [], $headers = [])
+    {
+        
+    }
+    public function methodNotAllowed($data = [], $headers = [])
+    {
+        
+    }
+    public function notAcceptable($data = [], $headers = [])
+    {
+        
+    }
+    public function conflict($data = [], $headers = [])
+    {
+        
+    }
+    public function lengthRequired($data = [], $headers = [])
+    {
+        
+    }
+    public function preconditionFailed($data = [], $headers = [])
+    {
+        
+    }
+    public function requestEntityTooLarge($data = [], $headers = [])
+    {
+        
+    }
+    public function unsupportedMediaType($data = [], $headers = [])
+    {
+        
+    }
+    public function tooManyRequests($data = [], $headers = [])
+    {
+        
     }
 
 }
